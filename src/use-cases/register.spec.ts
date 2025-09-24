6f2ceac60bdd29b2fa3739385802f821c05d4171
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-09-24 18:40:01 -0300

    feat: finishing to implement the units test about User Register

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,28 +1,32 @@
 import {expect, it, describe} from 'vitest'
+import {InMemoryUsersRepository} from 'src/repositories/in-memory/in-memory-users-repository.js'
 import { RegisterUseCase } from './register.js'
-import { PrismaUserRepository } from '@/repositories/prisma/prisma-user-repository.js'
 import { compare } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error.js'
 
 describe('Register Use-Case', () =>{
 
     it('should hash user password upon registration', async () =>{
-        const registerUseCase = new RegisterUseCase({
-            async findByEmail(email){
-                return null
-            },
+        const userRepository = new InMemoryUsersRepository()
+        const registerUser = new RegisterUseCase(userRepository)
 
-            async create(data){
-                return {
-                    id: 'user-1',
-                    name: data.name,
-                    email: data.email,
-                    password_hash: data.password_hash,
-                    created_at: new Date()
-                }
-            } 
+        const {user} = await registerUser.execute({
+            name: 'Jhon Doe',
+            email: 'johndoe@example.com',
+            password: '123456'
         })
 
-        const {user} = await registerUseCase.execute({
+        expect(user.id).toEqual(expect.any(String))
+        //aqui eu digo basicamente que eu espero que o user id seja igual a qualquer string
+    })
+
+
+
+    it('should hash user password upon registration', async () =>{
+        const userRepository = new InMemoryUsersRepository()
+        const registerUser = new RegisterUseCase(userRepository)
+
+        const {user} = await registerUser.execute({
             name: 'Jhon Doe',
             email: 'johndoe@example.com',
             password: '123456'
@@ -35,6 +39,31 @@ describe('Register Use-Case', () =>{
         //aqui eu faço a comparação se o valor que eu se assemelha ao hash
         
         expect(isPassowrdCorrectlyHashed).toBe(true)
+    })
+
+
+    it('should not be able to register with same email twice', async () =>{
+        const userRepository = new InMemoryUsersRepository()
+        const registerUser = new RegisterUseCase(userRepository)
+
+        const email = 'johndoe@example.com'
+
+        await registerUser.execute({
+            name: 'Jhon Doe',
+            email,
+            password: '123456'
+        })
+
+        expect(() =>
+            registerUser.execute({
+                name: 'Jhon Doe',
+                email,
+                password: '123456'
+            }),
+        ).rejects.toBeInstanceOf(UserAlreadyExistsError)
+        //neste rejects é basicamente o raises do python
+        //eu espero que dÊ algum erro nessa execução e posso dizer até mesmo
+        // qual dos meus erro que ele é, usando o isInstanceOf
     })
 
 })
