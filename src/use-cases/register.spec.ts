96d4d6d114a4c7f9e7379adac36c7243759605cb
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-09-24 18:51:29 -0300

    config: adding coverage script to tests

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -54,7 +54,7 @@ describe('Register Use-Case', () =>{
             password: '123456'
         })
 
-        expect(() =>
+        await expect(() =>
             registerUser.execute({
                 name: 'Jhon Doe',
                 email,
