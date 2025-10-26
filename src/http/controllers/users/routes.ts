d8bf2b6bdf435ebdaa2fae8400e61ae57771b2ec
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-10-26 19:19:04 -0300

    feat: implementing gyms controller and separating user controllers with their tests

---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,14 @@
+import type { FastifyInstance } from "fastify";
+import { register } from "./register.js";
+import { authenticate } from "./authenticate.js";
+import { profile } from "./profile.js";
+import { verifyJWT } from "../../middlewares/verify-jwt.js";
+
+export async function usersRoutes(app: FastifyInstance){
+    app.post('/users', register)
+    app.post('/sessios', authenticate)
+    
+    
+    /** Autenticated */
+    app.get('/me',{onRequest: [verifyJWT]}, profile)
+}
