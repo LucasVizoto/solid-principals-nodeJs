114f11f5359cf4b3732d68dbef56a1f0d2fa0409
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-10-26 20:44:18 -0300

    feat: implementing check-ins routes and controllers and in addition, fixing neraby and serch controllers in gyms routes to get informations on query and not into body

---
+++ src/http/controllers/check-ins/create.ts
@@ -0,0 +1,33 @@
+import type { FastifyRequest, FastifyReply } from 'fastify'
+import {z} from 'zod'
+import { makeCheckInUseCase } from '@/use-cases/factories/make-check-in-use-case.js'
+
+export async function create (request: FastifyRequest, reply: FastifyReply) {
+    const createCheckInParamsSchema = z.object({
+        gymId: z.string().uuid(),
+    })
+    
+    const createCheckInBodySchema = z.object({
+        latitude: z.number().refine(value =>{
+            return Math.abs(value) <= 90
+        }),
+        longitude: z.number().refine(value =>{
+            return Math.abs(value) <= 180
+        })
+    })
+
+    const {gymId} = createCheckInParamsSchema.parse(request.params)
+    const {latitude, longitude} = createCheckInBodySchema.parse(request.body)
+    
+        
+    const createCheckInUseCase  = makeCheckInUseCase()
+    
+    await createCheckInUseCase.execute({
+        gymId,
+        userId: request.user.sub, //como meu usuário já está autenticado, pego as informações do cookie
+        userLatitude: latitude,
+        userLongitude: longitude
+    })
+
+    return reply.status(201).send()
+}
